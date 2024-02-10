e47f188f27e468789248a0f69ad85dbfd2cd69d7
Mashio Sano <52710925+sano11o1@users.noreply.github.com>
2024-02-10 17:26:47 +0900

    装備なしの状態をNullではなく定数で表現する

--- chapter09/null_problem/good_code/main.go
+++ chapter09/null_problem/good_code/main.go
@@ -1,1 +1,52 @@
 package main
+
+import "errors"
+
+func main() {
+
+}
+
+type EquipmentName string
+
+const EQUIPMENT_NOTING EquipmentName = "Noting"
+const EQUIPMENT_HEAD EquipmentName = "Iron Helmet"
+
+type Equipment struct {
+	Name    EquipmentName
+	Defence int
+}
+
+func NewEquipment(name EquipmentName, defence int) (Equipment, error) {
+	if name == "" {
+		return Equipment{}, errors.New("name is empty")
+	}
+	return Equipment{
+		Name:    name,
+		Defence: defence,
+	}, nil
+}
+
+func NewNotingEquipment() Equipment {
+	return Equipment{Name: "Noting", Defence: 0}
+}
+
+type Member struct {
+	Head    Equipment
+	Body    Equipment
+	Arm     Equipment
+	Defence int
+}
+
+func (m Member) TotalDefence() int {
+	d := m.Defence
+	d += m.Head.Defence
+	d += m.Body.Defence
+	d += m.Arm.Defence
+	return d
+}
+
+func (m *Member) takeOfAllEquipment() {
+	m.Head = NewNotingEquipment()
+	m.Body = NewNotingEquipment()
+	m.Arm = NewNotingEquipment()
+}
