3328e545f5a42fc3aab70f806f988121b017ba5d
Mashio Sano <52710925+sano11o1@users.noreply.github.com>
2024-02-10 14:14:46 +0900

    ばかでかいManager構造体を解散！！

--- chapter08/Discount/good_code/main.go
+++ chapter08/Discount/good_code/main.go
@@ -1,1 +1,46 @@
 package main
+
+func main() {}
+
+type RegularPrice struct {
+	Amount int
+}
+
+const MIN_AMOUNT = 10000
+
+func NewRegularPrice(amount int) RegularPrice {
+	if amount < MIN_AMOUNT {
+		return RegularPrice{Amount: MIN_AMOUNT}
+	}
+	return RegularPrice{Amount: amount}
+}
+
+const REGULAR_DISCOUNT_MIN_AMOUNT = 10000
+const REGULAR_DISCOUNT_AMOUNT = 400
+
+type RegularDiscountedPrice struct {
+	Amount int
+}
+
+func NewRegularDiscountedPrice(regularPrice RegularPrice) RegularDiscountedPrice {
+	discountedAmount := regularPrice.Amount - REGULAR_DISCOUNT_AMOUNT
+	if discountedAmount < REGULAR_DISCOUNT_MIN_AMOUNT {
+		discountedAmount = REGULAR_DISCOUNT_MIN_AMOUNT
+	}
+	return RegularDiscountedPrice{Amount: discountedAmount}
+}
+
+const SUMMER_DISCOUNT_MIN_AMOUNT = 10000
+const SUMMER_DISCOUNT_AMOUNT = 300
+
+type SummerDiscountedPrice struct {
+	Amount int
+}
+
+func NewSummerDiscountedPrice(regularPrice RegularPrice) RegularDiscountedPrice {
+	discountedAmount := regularPrice.Amount - SUMMER_DISCOUNT_AMOUNT
+	if discountedAmount < SUMMER_DISCOUNT_MIN_AMOUNT {
+		discountedAmount = SUMMER_DISCOUNT_MIN_AMOUNT
+	}
+	return RegularDiscountedPrice{Amount: discountedAmount}
+}
